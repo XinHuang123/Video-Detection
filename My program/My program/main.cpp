cfc9926ab05d61c22df8a67f13dc0ec757fbe84c
Mark <Mark@Marks-MacBook-Air.local>
2015-02-11 14:36:12 -0500

    Initial Commit

---
+++ My program/My program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  My program
+//
+//  Created by Mark on 2/11/15.
+//  Copyright (c) 2015 Mark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
